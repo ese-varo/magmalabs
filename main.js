ac1bcd465a5fd8680576efc2e29826606bf6e228
varo <varoboss.ap@gmail.com>
2019-11-30 10:15:10 -0600

    Add nav hover functionality

--- main.js
+++ main.js
@@ -12,5 +12,13 @@ let h = window.innerHeight;
 const hero = document.querySelector('#hero');
 const exceptional = document.querySelector('#exceptional');
 const conceptToSolution = document.querySelector('#conceptToSolution'); 
+const contact = document.querySelector("#contact");
+const contactTxt = document.querySelector("#contact a");
 
 hero.style.height = h + "px";
+contact.addEventListener("mouseover", () => {
+  contactTxt.style.color = 'white';
+});
+contact.addEventListener("mouseout", () => {
+  contactTxt.style.color = '#F84848';
+});
